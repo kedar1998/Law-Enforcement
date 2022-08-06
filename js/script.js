40c345c1b719918304f98e625c0c7ed0dc92d588
Kedar <kedarmakode1598@gmail.com>
2022-08-06 15:33:23 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
+btn.addEventListener('click' , function(){
+    btn.classList.toggle('open');
+    menu.classList.toggle('hidden');
+})
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {screens:{
+    sm:'480px',
+    md:'768px',
+    lg:'976px',
+    lg:'1440px'
+  },
+    extend: {
+      colors:{
+        primary:'#2b2922',
+        secondary:'#88867f',
+        tertiary: '#867831'
+      }
+    },
+  },
+  plugins: [],
+}
