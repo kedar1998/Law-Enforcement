8ea65f42caeaa953ba1245870ce6ad57e1af1b4d
Kedar <kedarmakode1598@gmail.com>
2022-08-26 14:57:46 +0530

    added readme.md

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html"],
-  theme: {screens:{
+  theme: {
+    screens:{
     sm:'480px',
     md:'768px',
     lg:'976px',
